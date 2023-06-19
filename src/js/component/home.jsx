ed40801d3caceed1053063b3f02073f22ff96428
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-06-19 19:35:30 +0000

    add set and get methods

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,63 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const[todos, setTodos] = useState(() => {
+	
+		const savedTodos = localStorage.getItem("todos");
+		if (savedTodos) {
+		  return JSON.parse(savedTodos);
+		} else {
+		  return [];
+		}
+	  });
+
+	  const[inputValue, setInputValue] = useState("");
+	
+	
+	  useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	  }, [todos]);
+
+	const handleChange = (event) => {
+		setInputValue(event.target.value);
+	  };
+	
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			const newTodos = [...todos,inputValue]
+			setTodos(newTodos);
+			setInputValue("")
+		}
+	  };
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todo List</h1>
+			<ul>
+				<li>
+					<input type="text"
+					value={inputValue}
+					onChange={handleChange}
+					onKeyDown={handleKeyDown}
+					placeholder="Tasks To Be Done"></input>
+					</li>
+				{todos.map((item, index) =>
+					<li>{item}{""} <i className="fas fa-trash-alt"
+					onClick={() =>
+					setTodos(
+						todos.filter(
+							(t,currentIndex) =>
+							index != currentIndex
+						)
+					)
+				}
+					></i></li>
+				)
+
+				}
+				
+			</ul>
+			<div>{todos.length===0 ? <h4>No tasks, add one!</h4> : <h5>You have {todos.length} tasks to complete</h5>}</div>
 		</div>
 	);
 };
